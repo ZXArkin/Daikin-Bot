ed7fb375f7ef915c98c4a3f8d4080231a048e483
ZXByte <ltdat2001alt@gmail.com>
2018-05-17 17:20:28 +0700

    update help cmd...

--- Commands/help.js
+++ Commands/help.js
@@ -5,9 +5,7 @@ exports.run = (client, msg, args) => {
           name: `${client.user.username}`,
           icon_url: client.user.avatarURL
         },
-        description: `You can use these commands in **${msg.guild.name}** with the prefix \`%\`.
-Contact \`BriciWorld#1434\` if you need any help using the bot.
-You can also join [Sapphire's Home](https://discord.gg/DqGgueq) for help.`
+        description: `You can use these commands in **${msg.guild.name}** with the prefix \`!\`.`
     }
     });
 msg.author.send({embed: {
