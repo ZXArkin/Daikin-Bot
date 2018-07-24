1dc685806e0d484901247c30792acb8abed195c3
ZXByte <ltdat2001alt@gmail.com>
2018-07-24 17:11:01 +0700

    update ping command

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (client, msg, args) => {
     msg.channel.send(`Please wait until we check the bot's latency...`).then(function(m) {
-        m.edit(`:ping_pong: **Ping** - ${m.createdTimestamp - msg.createdTimestamp}ms.
+        m.edit(`:ping_pong: **Pong!** - ${m.createdTimestamp - msg.createdTimestamp}ms.
 :heartpulse: **Heartbeat** - ${Math.round(client.ping)}ms`);
     console.log(`[info] Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
     })
