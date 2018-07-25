0880d3fa94b40ea75290b54585a0ce218559f408
ZXByte <ltdat2001alt@gmail.com>
2018-07-25 16:51:58 +0700

    fixing profile command and add avatar command

--- Commands/profile.js
+++ Commands/profile.js
@@ -1,31 +1,65 @@
+const Discord = require("discord.js");
+
 exports.run = (client, msg, args) => {
-    msg.channel.send({embed: {
-        color: 0x194596,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
-        description: `Information for **${msg.author.username}**`,
+  const user = msg.mentions.users.first()
+    
+    if(user) {
+      msg.channel.send({embed: {
+        color: 0xd45555,
+        title: `:bust_in_silhouette: User information:`,
         thumbnail: {
-          url: msg.author.avatarURL
+          url: user.avatarURL
            },
           fields: [{
-            name: ":hash: Discord Tag",
-            value: `${msg.author.tag}`
+            name: "Username & Discriminator",
+            value: `@**${user.tag}**`
           },
           {
-            name: ":calendar_spiral: Account created",
-            value: `${msg.author.createdAt}`,
+            name: "Created",
+            value: `${user.createdAt}`
+          },
+          {
+            name: "Bot?",
+            value: `${user.bot}`,
             inline: true
           },
           {
             name: ":id:",
-            value: `${msg.author.id}`,
+            value: `${user.id}`,
             inline: true
           },
         ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: msg.author.avatarURL,
-          text: `Requested by ${msg.author.tag}`
-            }
+        }
+    });
+    } else {
+msg.channel.send({embed: {
+    color: 0xd45555,
+    title: `:bust_in_silhouette: User information:`,
+    thumbnail: {
+      url: msg.author.avatarURL
+       },
+      fields: [{
+        name: "Username & Discriminator",
+        value: `@**${msg.author.tag}**`
+      },
+      {
+        name: "Created",
+        value: `${msg.author.createdAt}`
+      },
+      {
+        name: "Bot?",
+        value: `${msg.author.bot}`,
+        inline: true
+      },
+      {
+        name: ":id:",
+        value: `${msg.author.id}`,
+        inline: true
+      },
+    ],
+    }
+});
+    }
+console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
+};
+config: {}
